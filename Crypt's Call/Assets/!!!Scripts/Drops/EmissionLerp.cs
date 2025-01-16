84a05ccfdf72f2be98572198e0811a70d892c602
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2025-01-16 15:13:37 +0200

    Drop Emission

---
+++ Crypt's Call/Assets/!!!Scripts/Drops/EmissionLerp.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class EmissionLerp : MonoBehaviour
+{
+    public float lerpSpeed = 1f;
+
+    private List<MeshRenderer> meshRenderers = new List<MeshRenderer>();
+
+    private Dictionary<Material, Color> originalEmissionColors = new Dictionary<Material, Color>();
+
+    void Start()
+    {
+        meshRenderers.AddRange(GetComponentsInChildren<MeshRenderer>());
+
+        foreach (MeshRenderer renderer in meshRenderers)
+        {
+            foreach (Material mat in renderer.materials)
+            {
+                if (mat.HasProperty("_EmissionColor"))
+                {
+                    mat.EnableKeyword("_EMISSION");
+
+                    if (!originalEmissionColors.ContainsKey(mat))
+                    {
+                        originalEmissionColors.Add(mat, mat.GetColor("_EmissionColor"));
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning($"Material {mat.name} does not have an _EmissionColor property.");
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        float lerpFactor = (Mathf.Sin(Time.time * lerpSpeed) + 1f) / 2f;
+
+        foreach (MeshRenderer renderer in meshRenderers)
+        {
+            foreach (Material mat in renderer.materials)
+            {
+                if (originalEmissionColors.ContainsKey(mat))
+                {
+                    Color targetColor = Color.Lerp(originalEmissionColors[mat], Color.white, lerpFactor);
+                    mat.SetColor("_EmissionColor", targetColor);
+                }
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        foreach (MeshRenderer renderer in meshRenderers)
+        {
+            foreach (Material mat in renderer.materials)
+            {
+                if (originalEmissionColors.ContainsKey(mat))
+                {
+                    mat.SetColor("_EmissionColor", originalEmissionColors[mat]);
+                }
+            }
+        }
+    }
+}
